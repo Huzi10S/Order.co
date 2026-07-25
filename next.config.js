c8d3cb589aa0ae2d26926b0d72f09ee3ee0cf55f
Huzefa Haveliwala <hatts365@gmail.com>
2026-07-26 02:10:08 +0530

    Restore client side env mappings and server side bypass

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'dist',
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.apiKey || process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.authDomain || process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.projectId || process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.storageBucket || process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.messagingSenderId || process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.appId || process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  },
 };
 
 module.exports = nextConfig;
